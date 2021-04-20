0cbc5d9c57f27682a7557b281808a5eeaba75232
tipsi2022 <77313485+tipsi2022@users.noreply.github.com>
2021-04-20 22:04:40 +0530

    Delete LOL directory

--- LOL/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from django.contrib.auth import views as auth
-from . import views 
-
-urlpatterns = [
-    path('login/', auth.LoginView.as_view(template_name ='accounts/login.html'), name ='login'),
-    path('logout/', auth.LogoutView.as_view(template_name ='accounts/logout.html'), name ='logout'),
-    path('signup/', views.signup, name ='signup'),
-]
